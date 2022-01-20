8903e3470c6edb27bbfd90acfa45e37afc46e863
Stéphanie Corrias <scminos@gmail.com>
2022-01-20 17:44:15 +0100

    maj modele sauce

--- models/Sauce.js
+++ models/Sauce.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 
 const sauceSchema = mongoose.Schema ({
+  userId : {type: String, require: true},
   name : {type: String, require: true},
   manufacturer : {type: String, require: true},
   description : {type: String, require: true},
+  mainPepper: {type: String, require: true},
   imageUrl :{type: String, require: true},
-  userId : {type: String, require: true},
-  mainPepperIngredient : {type: String, require: true}, 
+  heat : {type: Number},
+  likes : {type: Number},
+  dislikes : {type: Number},
+  userLiked : {type : String},
+  userDisliked : {type : String}
 })
 
 module.exports = mongoose.model('Sauce', sauceSchema)
